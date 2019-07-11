4df19ff38a4d4342e62ebd4007763462d9372c42
Jesse Smidt <jesse.smidt@appfolio.com>
2019-07-11 13:07:29 -0700

    combine application controller tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,13 +1,10 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
-    get '/'
-    assert_response :success
-  end
-
   test 'should get home with body' do
     get '/'
-    assert_not_equal @response.body, nil, 'response did not have a body'
+
+    assert_response :success
+    assert_select 'h1', text: 'Some Content!'
   end
 end
