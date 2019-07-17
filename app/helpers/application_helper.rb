445b8c8fdbe7a8594e820ceb339f14354765cbba
Jesse Smidt <jesse.smidt@appfolio.com>
2019-07-16 17:03:24 -0700

    update css structure based on feedback

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def link_active?(route)
-    return 'active' if request.path == route
+  def css_class_for_active_link(route)
+    return 'active active--underline' if request.path == route
   end
 end
